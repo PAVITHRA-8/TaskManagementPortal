43189a4ad638ef094f2a2549bb235a6fc4f09fa2
Priyanjali <priyanjalibarriar091@gmail.com>
2023-06-26 15:54:19 +0530

    Verification page updated

--- TaskManagement/src/app/app-routing.module.ts
+++ TaskManagement/src/app/app-routing.module.ts
@@ -9,14 +9,16 @@ import { ProfileComponent } from './profile/profile.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { TaskDetailsComponent } from './task-details/task-details.component';
 import { SummaryComponent } from './summary/summary.component';
+import { VerificationComponent } from './verification/verification.component';
 
 const routes: Routes = [
 
   { path:'task', component: TaskComponent},
   { path:'sidenav', component:SidenavComponent},
-  {path:'create-task',component:CreateTaskComponent},
-  {path:'user',component:UserComponent},
-  {path:'profile',component:ProfileComponent}
+  { path:'create-task',component:CreateTaskComponent},
+  { path:'user',component:UserComponent},
+  { path:'profile',component:ProfileComponent},
+  { path:'verification',component:VerificationComponent}
   
 ];
 
